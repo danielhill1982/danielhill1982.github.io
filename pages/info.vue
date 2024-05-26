5f8ed9bb76bf2d9ee432577654bbd51e1a0d9f39
danielhill1982 <mail@dhill.dev>
2024-05-26 20:17:07 +0200

    final

--- pages/info.vue
+++ pages/info.vue
@@ -1,44 +1,75 @@
 <template>
-<div>
+  <div>
 
-  <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10">
-    <div class="max-w-2xl text-left mx-auto">
-      <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Meine Website und digitale, visuelle Plattform, mit <span class="text-sky-400">Informationen</span> zu dieser Internetseite, zu meiner <span class="text-sky-400">Person</span>, ausgewählte <span class="text-sky-400">Tribute</span> von Technologien und Unternehmen, die ich benutze, oder deren Dienstleistungen ich in anspruch nehme.</p>
-      <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Ich bin ein Fotoshooter, Geschichtenerzähler, Designschöpfer, Softwareentwickler und Gameplayer, Musikhörer, Fahrradfahrer, Bahnreisender und Wohnungsmieter aus Europa mit Sitz in Deutschland, Thüringen.
-</p>
-<p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Meine Computerkenntnisse vertiefte ich seit 1999 als Mediengestalter mit den Schwerpunkten Nonprint-Design und World Wide Web. Durch die Arbeit und den Austausch mit Softwareentwicklern habe ich gelernt, wie man Markenwebsites selbstständig implementiert.</p>
-      
-<h2 class="block text-2xl font-bold text-gray-800 sm:text-3xl md:text-4xl dark:text-white mt-10">Steckbrief</h2>
-<p class="mt-3 text-lg text-gray-800 dark:text-gray-400">
-  Daniel Hill<br>
-Cuno-Hoffmeister-Straße 18<br>
-96515 Sonneberg</p>
-<NuxtLink class="font-semibold text-lg text-sky-400 hover:text-sky-500 duration-500" to="mailto:danielhill@tutamail.com" alt="" title="">danielhill@tutamail.com</NuxtLink>
-<p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Daniel ist 179 cm groß,<br>hat dunkle Haare,<br>grüne Augen,<br>Tätowierungen und<br>wiegt 95 Kilogramm.</p>
-<p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Sprachen: Deutsch, Englisch</p>
+    <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10">
+      <div class="max-w-2xl text-left mx-auto">
+        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Meine Website und digitale, visuelle Plattform, mit
+          <span class="text-sky-400">Informationen</span> zu dieser Internetseite, zu meiner <span
+            class="text-sky-400">Person</span>, ausgewählte <span class="text-sky-400">Tribute</span> von Technologien
+          und Unternehmen, die ich benutze, oder deren Dienstleistungen ich in anspruch nehme.</p>
+        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Ich bin ein Fotoshooter, Geschichtenerzähler,
+          Designschöpfer, Softwareentwickler und Gameplayer, Musikhörer, Fahrradfahrer, Bahnreisender und Wohnungsmieter
+          aus Europa mit Sitz in Deutschland, Thüringen.
+        </p>
+        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Meine Computerkenntnisse vertiefte ich seit 1999 als
+          Mediengestalter mit den Schwerpunkten Nonprint-Design und World Wide Web. Durch die Arbeit und den Austausch
+          mit Softwareentwicklern habe ich gelernt, wie man Markenwebsites selbstständig implementiert.</p>
 
-<h2 class="block text-2xl font-bold text-gray-800 sm:text-3xl md:text-4xl dark:text-white mt-10">Öffentliche Profile</h2>
-<ul class="list-none text-2xl mb-20 mt-3">
-        <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://codepen.io/danielhill1982" class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title="" target="_blank">codepen</a>
-        </li>
-        <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://danielhill.vercel.app" class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title="" target="_blank">vercel</a>
-        </li>
-        <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://discord.gg/qSYNdQvrk9" class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title="" target="_blank">discord</a>
-        </li>
-        <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://github.com/danielhill1982" class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title="" target="_blank">github</a>
-        </li>
-        <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://danielhill1982.github.io/" class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title="" target="_blank">github pages</a>
-        </li>
-        <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://gog.com/u/schwuppi1982" class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title="" target="_blank">good old games</a>
-        </li>
-        <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://steamcommunity.com/id/danielhill1982/" class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title="" target="_blank">steam</a>
-        </li>
-        <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="http://paypal.com/paypalme/danielhill1982" class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title="" target="_blank">paypal</a>
-        </li>
-      </ul>
+        <h2 class="block text-2xl font-bold text-gray-800 sm:text-3xl md:text-4xl dark:text-white mt-10">Steckbrief</h2>
+        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">
+          Daniel Hill<br>
+          Cuno-Hoffmeister-Straße 18<br>
+          96515 Sonneberg</p>
+        <NuxtLink class="font-semibold text-lg text-sky-400 hover:text-sky-500 duration-500"
+          to="mailto:danielhill@tutamail.com" alt="" title="">danielhill@tutamail.com</NuxtLink>
+        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Daniel ist 179 cm groß,<br>hat dunkle Haare,<br>grüne
+          Augen,<br>Tätowierungen und<br>wiegt 95 Kilogramm.</p>
+        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Sprachen: Deutsch, Englisch</p>
+
+        <h2 class="block text-2xl font-bold text-gray-800 sm:text-3xl md:text-4xl dark:text-white mt-10">Öffentliche
+          Profile</h2>
+        <ul class="list-none text-2xl mb-10 mt-3">
+          <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://codepen.io/danielhill1982"
+              class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title=""
+              target="_blank">codepen</a>
+          </li>
+          <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://discord.gg/qSYNdQvrk9"
+              class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title=""
+              target="_blank">discord</a>
+          </li>
+          <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://github.com/danielhill1982"
+              class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title=""
+              target="_blank">github</a>
+          </li>
+          <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://gog.com/u/schwuppi1982"
+              class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title="" target="_blank">good
+              old games</a>
+          </li>
+          <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a
+              href="https://steamcommunity.com/id/danielhill1982/"
+              class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title=""
+              target="_blank">steam</a>
+          </li>
+          <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="http://paypal.com/paypalme/danielhill1982"
+              class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title=""
+              target="_blank">paypal</a>
+          </li>
+        </ul>
+
+        <h2 class="block text-2xl font-bold text-gray-800 sm:text-3xl md:text-4xl dark:text-white mt-10">Websites</h2>
+        <ul class="list-none text-2xl mb-10 mt-3">
+          <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://danielhill1982.github.io/"
+              class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title=""
+              target="_blank">danielhill1982.github.io</a>
+          </li>
+          <li class="pb-3 text-lg text-gray-800 dark:text-gray-400"><a href="https://danielhill.vercel.app"
+              class="font-semibold text-sky-400 hover:text-sky-500 duration-500" alt="" title=""
+              target="_blank">danielhill.vercel.app</a>
+          </li>
+        </ul>
+      </div>
     </div>
+
+
   </div>
-
-
-</div>
 </template>
