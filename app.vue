3b1182816eef4d1ac1c65d8584682fe51d2596d0
danielhill1982 <mail@dhill.dev>
2024-05-27 07:03:09 +0200

    final

--- app.vue
+++ app.vue
@@ -78,7 +78,7 @@
     <footer>
 
       <div class="mt-auto w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto text-center">
-        
+
 
 
         <div class="titles dark:bg-white bg-gray-200 p-10">
@@ -97,8 +97,14 @@
         </div>
         <div class="mt-6">
           <input data-hs-theme-switch=""
-          class="relative w-[3.25rem] h-7 mt-1 mr-1 bg-gray-100 checked:bg-none checked:bg-sky-400 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 ring-1 ring-transparent focus:border-slate-700 focus:ring-slate-700 focus:outline-none appearance-none before:inline-block before:size-6 before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:shadow before:rounded-full before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 after:absolute after:end-1.5 after:top-[calc(50%-0.40625rem)] after:w-[.8125rem] after:h-[.8125rem] after:bg-no-repeat after:bg-[right_center] after:bg-[length:.8125em_.8125em] after:transform after:transition-all after:ease-in-out after:duration-200 after:opacity-70 checked:after:start-1.5 checked:after:end-auto"
-          type="checkbox" id="darkSwitch">
+            class="relative w-[3.25rem] h-7 mt-1 mr-1 bg-gray-100 checked:bg-none checked:bg-sky-400 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 ring-1 ring-transparent focus:border-slate-700 focus:ring-slate-700 focus:outline-none appearance-none before:inline-block before:size-6 before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:shadow before:rounded-full before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 after:absolute after:end-1.5 after:top-[calc(50%-0.40625rem)] after:w-[.8125rem] after:h-[.8125rem] after:bg-no-repeat after:bg-[right_center] after:bg-[length:.8125em_.8125em] after:transform after:transition-all after:ease-in-out after:duration-200 after:opacity-70 checked:after:start-1.5 checked:after:end-auto"
+            type="checkbox" id="darkSwitch">
+        </div>
+        <div class="my-10">
+          <audio controls class="bg-gray-200 w-full">
+            <source src="/Der Dritte Raum - Hale Bopp (Ricardo Villalobos Remix).mp3" type="audio/mpeg">
+            Your browser does not support the audio tag.
+          </audio>
         </div>
 
 
--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
 
        
         <div class="max-w-2xl p-10">
-          <h1 class="block leading-tight align-text-top text-4xl bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent dark:from-blue-400 dark:to-violet-400">
+          <h1 class="block p-5 border-4 border-gray-200 border-dashed leading-tight align-text-top text-4xl bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent dark:from-blue-400 dark:to-violet-400">
             Intuitive Webapplikationen
           </h1>
         </div>
@@ -26,7 +26,7 @@
        
         <div class="mt-6 mb-60 gap-3 flex justify-center">
           <NuxtLink class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-sky-400 text-white hover:bg-sky-500 disabled:opacity-50 disabled:pointer-events-none duration-500" to="/info">
-            Weitere Informationen
+            Weiter zur nächsten Seite
             <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
           </NuxtLink>
         </div>
