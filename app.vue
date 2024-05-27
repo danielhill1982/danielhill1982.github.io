1accb318a47fa73c7d7c94705872fbefd68d0e06
danielhill1982 <mail@dhill.dev>
2024-05-27 15:42:32 +0200

    final

--- app.vue
+++ app.vue
@@ -30,7 +30,7 @@
           </div>
         </div>
         <div id="navbar-with-collapse"
-          class="hidden overflow-hidden basis-full grow sm:block mt-5">
+          class="hidden transition-all duration-[0.1] overflow-hidden basis-full grow sm:block mt-5">
           <div>
             <ul class="flex flex-col gap-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
               <li>
