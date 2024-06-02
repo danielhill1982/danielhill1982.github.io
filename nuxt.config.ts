7f8cd96267304838f4e6f33a71a30f691c7000f4
danielhill1982 <mail@dhill.dev>
2024-06-03 00:05:24 +0200

    final

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/fonts"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/fonts", "@nuxt/image"],
 
   plugins: ["~/plugins/preline.client.ts"],
   
