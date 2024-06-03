8723cd6b3da351fbc4b463c8d0dce09343e05aab
danielhill1982 <mail@dhill.dev>
2024-06-03 09:39:13 +0200

    final

--- pages/dienstleistung.vue
+++ pages/dienstleistung.vue
@@ -1,50 +1,20 @@
 <template>
+    
+        <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 my-10">
 
-    <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 my-10">
+            <div class="max-w-2xl text-left mx-auto">
+                <p class="text-lg text-gray-800 dark:text-gray-400">Ich biete ein umfassendes Spektrum an
+                    Design-Dienstleistungen an, um Ihre Vision zum Leben zu erwecken.</p>
+                <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Von der ersten Beratung und Konzeptentwicklung
+                    über die Erstellung von Logos und UI/UX-Design bis hin zur Softwareentwicklung decke ich alle
+                    Aspekte Ihres Projekts ab.</p>
+                <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Mein Ziel ist es, innovative Lösungen zu
+                    liefern, die Ihren Anforderungen gerecht werden und Ihre Erwartungen übertreffen.</p>
 
-        <div class="max-w-2xl text-left mx-auto">
-            <p class="text-lg text-gray-800 dark:text-gray-400">Ich biete ein umfassendes Spektrum an
-                Design-Dienstleistungen an, um Ihre Vision zum Leben zu erwecken.</p>
-            <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Von der ersten Beratung und Konzeptentwicklung
-                über die Erstellung von Logos und UI/UX-Design bis hin zur Softwareentwicklung decke ich alle
-                Aspekte Ihres Projekts ab.</p>
-            <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Mein Ziel ist es, innovative Lösungen zu
-                liefern, die Ihren Anforderungen gerecht werden und Ihre Erwartungen übertreffen.</p>
+                    
 
-            <div class="mt-3 hs-dropdown relative inline-flex">
-                <button id="hs-dropdown-default" type="button"
-                    class="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
-                    Navi
-                    <svg class="hs-dropdown-open:rotate-180 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
-                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path d="m6 9 6 6 6-6" />
-                    </svg>
-                </button>
 
-                <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700 dark:divide-neutral-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full"
-                    aria-labelledby="hs-dropdown-default">
-                    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
-                        href="#">
-                        Newsletter
-                    </a>
-                    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
-                        href="#">
-                        Purchases
-                    </a>
-                    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
-                        href="#">
-                        Downloads
-                    </a>
-                    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
-                        href="#">
-                        Team Account
-                    </a>
-                </div>
             </div>
-
-
         </div>
-    </div>
-
+    
 </template>
