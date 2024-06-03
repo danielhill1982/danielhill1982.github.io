9aee751ba5e364c382f8a39ea1bfd53ca1587104
danielhill1982 <mail@dhill.dev>
2024-06-03 10:46:27 +0200

    final

--- pages/dienstleistung.vue
+++ pages/dienstleistung.vue
@@ -1,20 +1,26 @@
 <template>
-    
-        <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 my-10">
 
-            <div class="max-w-2xl text-left mx-auto">
-                <p class="text-lg text-gray-800 dark:text-gray-400">Ich biete ein umfassendes Spektrum an
-                    Design-Dienstleistungen an, um Ihre Vision zum Leben zu erwecken.</p>
-                <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Von der ersten Beratung und Konzeptentwicklung
-                    über die Erstellung von Logos und UI/UX-Design bis hin zur Softwareentwicklung decke ich alle
-                    Aspekte Ihres Projekts ab.</p>
-                <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Mein Ziel ist es, innovative Lösungen zu
-                    liefern, die Ihren Anforderungen gerecht werden und Ihre Erwartungen übertreffen.</p>
+    <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 my-10">
 
-                    
-
-
-            </div>
+        <div class="max-w-2xl text-left mx-auto">
+            <p class="text-lg text-gray-800 dark:text-gray-400">Ich biete ein umfassendes Spektrum an
+                Design-Dienstleistungen an, um Ihre Vision zum Leben zu erwecken.</p>
+            <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Von der ersten Beratung und Konzeptentwicklung
+                über die Erstellung von Logos und UI/UX-Design bis hin zur Softwareentwicklung decke ich alle
+                Aspekte Ihres Projekts ab.</p>
+            <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Mein Ziel ist es, innovative Lösungen zu
+                liefern, die Ihren Anforderungen gerecht werden und Ihre Erwartungen übertreffen.</p>
         </div>
-    
+
+        <div class="max-w-2xl text-left mx-auto">
+            <hr class="mt-12 border-4 border-gray-600 dark:border-gray-200 w-56">
+            <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">
+                
+                <NuxtLink class="font-semibold text-sky-400 hover:text-sky-500 duration-500" to="https://danielhill1982.github.io/greensock/" alt="" title="" target="_blank">GSAP Snippets</NuxtLink></p>
+                <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Unter diesem Link habe ich eine Sammlung von GSAP-Beispielen veröffentlicht, die als Grundlage für komplexe Animationen dienen. GSAP ist eine äußerst robuste und ressourcenschonende JavaScript-Animationsbibliothek für Profis.  
+                </p>
+            
+        </div>
+    </div>
+
 </template>
